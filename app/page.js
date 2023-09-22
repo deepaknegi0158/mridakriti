ecdef9544b948aa29cc5dd13880f78bbd7ea4dea
deepaknegi00 <deepak.negi0158@gmail.com>
2023-09-23 00:59:15 +0530

    logs-23-09-23 -v1

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 "use client"
+import { useState } from 'react';
+import Image from 'next/image'
+
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
 import Product from '../components/Product';
 import Footer from '../components/Footer';
 import Feedback from '../components/Feedback';
 import Join from '../components/Joinus';
-import { useState } from 'react';
+import Logo from '../public/Logo.png'
+
 import { useLoadingAnimation } from '../lib/LoadingAnimation';
 
 export default function Home() {
@@ -14,29 +18,56 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   useLoadingAnimation(setLoading);
 
-  return (
-    <main className='font-sans'>
-      {loading ? (
+  if (loading === true) {
+    return (
+      <main>
         <div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-lg z-50">
           <div className="logo">
-            <img
-              src="logo.png"
+            <Image
+              src={Logo}
               alt="mridakriti-logo"
+              priority='high'
             />
           </div>
         </div>
-      ) : (
-        <>
-          <Navbar sectionOffsets={sectionOffsets} />
-          <Slider />
-          <Product />
-          <Feedback />
-          <Join />
-          <Footer />
-        </>
-      )
-      }
+      </main>
+    )
+  } else {
+    return (
+      <main className='font-sans'>
+        <Navbar sectionOffsets={sectionOffsets} />
+        <Slider />
+        <Product />
+        <Feedback />
+        <Join />
+        <Footer />
+      </main>
+    )
+  }
 
-    </main>
-  )
+  // return (
+  //   <main className='font-sans'>
+  //     {loading ? (
+  //       <div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-lg z-50">
+  //         <div className="logo">
+  //           <Image
+  //             src={Logo}
+  //             alt="mridakriti-logo"
+  //           />
+  //         </div>
+  //       </div>
+  //     ) : (
+  //       <>
+  //         <Navbar sectionOffsets={sectionOffsets} />
+  //         <Slider />
+  //         <Product />
+  //         <Feedback />
+  //         <Join />
+  //         <Footer />
+  //       </>
+  //     )
+  //     }
+
+  //   </main>
+  // )
 }
